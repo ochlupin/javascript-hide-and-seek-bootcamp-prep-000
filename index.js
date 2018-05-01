350100589a7e4fffd3ba6fddb0bd7975cd89dea0
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-05-01 15:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function getFirstSelector(selector){
-  return(document.querySelector(selector));
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById("nested").querySelector(".target");
+  // or can be written as return docum
 }
