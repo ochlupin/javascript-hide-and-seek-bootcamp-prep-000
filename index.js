5685db2ebbccec9d50eac4309e9084ac1c02b1f3
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-05-01 15:23:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function nestedTarget(){
   return document.getElementById("nested").querySelector(".target");
   // or can be written as return document.querySelector('#nested .target')
 }
+
+function increaseRankBy(n){
+  const lists = document.querySelectorAll('.ranked-list')
+  for (let i=0;i<lists.length;i++){
+    lists[i].innerHTML=parseInt(lists[i].innerHTML) + n;
+  }
+}
