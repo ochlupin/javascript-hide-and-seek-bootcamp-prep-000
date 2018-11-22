b52d5073b7b169601efc714e8fc3494285956359
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-11-22 14:57:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-function getFirstSelector(selector){
+// getFirstSelector accepts one argument (selector) and returns the first element that matches the selector
+function getFirstSelector(selector) {
   return document.querySelector(selector);
-}
+};
 
-function nestedTarget(){
+// nestedTarget that pulls a .target out of #nested
+function nestedTarget() {
   return document.getElementById("nested").querySelector(".target");
-  // or can be written as return document.querySelector('#nested .target')
-}
+};
 
-function increaseRankBy(n){
-  const lists = document.querySelectorAll('.ranked-list')
-  for (let i=0;i<lists.length;i++){
-    lists[i].innerHTML=parseInt(lists[i].innerHTML) + n;
+//increaseRankBy(n) - increases the ranks in all of the .ranked-list by n
+// select all elements with class .ranked-list
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
   }
 }
 
-function deepestChild(){
-  var list=document.getElementById("grand-node").querySelectorAll("div");
-  var child;
-  for(let i=0;i<list.length-1;i++){
+// deepestChild() - pulls out the most deeply nested child element from  div#grand-node
+
+function deepestChild() {
+  // selects all the divs inside the #grand-node element
+  let list = document.querySelector(`#grand-node`).querySelectorAll("div");
+  let child;
+  //for loop that r uns as long as the length of the array list-1 is lower than the counter variable i, the loop assigns the variable `child` the return value of querySelector("div") and returns it after the loop is done running
+  for (let i = 0; i < list.length - 1; i++) {
     child = list[i].querySelector("div");
   }
-  return child
+  return child;
 }
